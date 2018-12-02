520ff91e54d3496f81945b56cb748e296380cf7e
Mikian Musser <Bobar312@gmail.com>
2018-12-02 11:56:55 -0800

    Added loop

--- type.js
+++ type.js
@@ -14,8 +14,9 @@ jQuery(document).ready(function() {
       // ' <div class="genAddTextPadding genDarkYellowHighlight typeBoxName"><h1>Mikian Musser</h1><p>(702)540-4190</p><p>Bobar312@gmail.com</p></div>'
     ],
     typeSpeed: 30,
-    backSpeed: 30,
-    backDelay: 30,
+    backSpeed: 40,
+    backDelay: 40,
     startDelay: 0,
+    loop: true
   });
 });
