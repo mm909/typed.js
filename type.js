bcb6b7e2d0225ea0b9267bb23b00eb4d32ecb182
Mikian Musser <Bobar312@gmail.com>
2018-12-02 14:16:57 -0800

    Updated look and text

--- type.js
+++ type.js
@@ -1,22 +1,17 @@
-let firstType = true;
 jQuery(document).ready(function() {
   var type = new Typed('#type', {
     strings: [
-      ' <span class="genAddTextPadding genBlueHighlight typeBoxName">Who I am?<span>', // 1
-      ' <span class="genAddTextPadding genBlueHighlight typeBoxName">I am ...!</span>', // 1
-      ' <span class="genAddTextPadding genRedHighlight typeBoxName">I am a Leader.</span>', // 3
-      ' <span class="genAddTextPadding genRedHighlight typeBoxName">I am a Code Central Member.</span>', // 3
-      ' <span class="genAddTextPadding genRedHighlight typeBoxName">I am a Gamer.</span>', // 3
-      ' <span class="genAddTextPadding genOrangeHighlight typeBoxName">I am a Programmer.</span>', // 3
-      ' <span class="genAddTextPadding genOrangeHighlight typeBoxName">I am a Volunteer.</span>', // 3
-      ' <span class="genAddTextPadding genOrangeHighlight typeBoxName">I am a Mentor.</span>', // 3
-      ' <span class="genAddTextPadding genYellowHighlight typeBoxName">I am Mikian Musser.</span>',
-      // ' <div class="genAddTextPadding genDarkYellowHighlight typeBoxName"><h1>Mikian Musser</h1><p>(702)540-4190</p><p>Bobar312@gmail.com</p></div>'
+      ' <span class="genAddTextPadding genBlueHighlight typeBoxName">This is an example of Typed.js</span>', // 1
+      ' <span class="genAddTextPadding genBlueHighlight typeBoxName">It is a free and open source JS library. </span>', // 1
+      ' <span class="genAddTextPadding genRedHighlight typeBoxName">That simulates typing on your website.</span>', // 3
+      ' <span class="genAddTextPadding genRedHighlight typeBoxName">You can <del>add some</del> <b>styles to your </b> text <i> as well. </i></span>', // 3
+      ' <span class="genAddTextPadding genRedHighlight typeBoxName">Want to learn how?</span>', // 3
+      ' <span class="genAddTextPadding genOrangeHighlight typeBoxName">Ask an instructor!</span>', // 3
+      ' <span class="genAddTextPadding genYellowHighlight typeBoxName">#Code{Central}</span>',
     ],
     typeSpeed: 30,
-    backSpeed: 40,
-    backDelay: 40,
-    startDelay: 0,
+    backSpeed: 20,
+    backDelay: 1000,
     loop: true
   });
 });
